c17b7effbe6838a7eda6237ab558f159993e2873
daniel.iwaniec <daniel.iwaniec@meinarztapp.de>
2024-05-25 19:05:09 +0200

    front controllers

--- app/serviced/public/index.php
+++ app/serviced/public/index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "test";
+use App\Kernel;
+
+$_SERVER['APP_RUNTIME_OPTIONS'] = [
+    'disable_dotenv' => true,
+];
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};

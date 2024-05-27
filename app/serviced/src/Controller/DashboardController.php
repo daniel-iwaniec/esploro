7f8d58af305ed19d55575281a769cdac5ea63acf
daniel.iwaniec <daniel.iwaniec@meinarztapp.de>
2024-05-27 12:39:34 +0200

    better csv processing

--- app/serviced/src/Controller/DashboardController.php
+++ app/serviced/src/Controller/DashboardController.php
@@ -8,6 +8,7 @@ use Exception;
 use App\Entity\Transaction;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,6 +17,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/dashboard', name: 'dashboard', methods: ['GET', 'POST'])]
 class DashboardController extends AbstractController
 {
+    private const int CSV_BATCH = 10;
+
     public function __construct(
         private readonly EntityManagerInterface $entityManager
     ) {
@@ -26,34 +29,40 @@ class DashboardController extends AbstractController
      */
     public function __invoke(Request $request): Response
     {
-        $data = [];
-        $total = 0;
         if ($request->isMethod('POST')) {
-            $file = $request->files->get('csv')->getContent();
+            $file = $request->files->get('csv');
+            assert($file instanceof UploadedFile);
 
-            $data = [];
-            $rows = str_getcsv($file, "\n");
-            foreach ($rows as $row) {
-                $data[] = str_getcsv($row);
-            }
+            $fileObject = $file->openFile();
 
-            foreach ($data as $item) {
+            for ($total = $count = 0; !$fileObject->eof();) {
+                $line = str_getcsv($fileObject->fgets());
+                if ($line === [null]) continue;
+
+                [$amount, $date, $description] = $line;
+
                 $transaction = new Transaction();
-                $transaction->setAmount((float) $item[0]);
-                $transaction->setDate(new DateTimeImmutable($item[1]));
-                $transaction->setDescription((string) $item[2]);
+                $transaction->setAmount((float) $amount);
+                $transaction->setDate(new DateTimeImmutable($date));
+                $transaction->setDescription($description);
+                $this->entityManager->persist($transaction);
 
+                $count++;
                 $total += $transaction->getAmount();
 
-                $this->entityManager->persist($transaction);
-                $this->entityManager->flush();
+                if (($count % self::CSV_BATCH) === 0) {
+                    $this->entityManager->flush();
+                    $this->entityManager->clear();
+                }
             }
+
+            $this->entityManager->flush();
+            $this->entityManager->clear();
         }
 
         return $this->render('dashboard.html.twig', [
-            'data' => $data,
-            'totalCount' => count($data),
-            'total' => $total
+            'total' => $total ?? 0,
+            'count' => $count ?? 0,
         ]);
     }
 }

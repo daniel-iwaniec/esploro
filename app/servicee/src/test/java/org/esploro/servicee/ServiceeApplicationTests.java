2ff2c58d8921fe6b8046606cd8b1eb5769649685
daniel.iwaniec <daniel.iwaniec@meinarztapp.de>
2024-06-14 13:03:52 +0200

    spring boot init

---
+++ app/servicee/src/test/java/org/esploro/servicee/ServiceeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esploro.servicee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
